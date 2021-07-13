86e41735a841745c60a5df981b3677e2425e7d4c
Ricardo Pereira Marques <ricardo80@gmail.com>
2021-07-13 10:53:36 -0300

    Modificando dados em cada página

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,21 +2,37 @@ import { Request, Response } from 'express';
 
 // export é pra que possa ser usando em outros arquivos.
 export const home = (req: Request, res: Response) => {
-    //res.send('HOME NO CONTROLLER');
-    res.render('pages/page');
+    res.render('pages/page',{
+        banner:{
+            titulo:'Todos os animais',
+            imagem_bg:'allanimals.jpg'
+        }
+    });
 };
 
 export const cachorros = (req: Request, res: Response) => {
-    res.send('CACHORROS NO CONTROLLER');
-    //res.render('pages/page');
+    res.render('pages/page',{
+        banner:{
+            titulo:'Todos os Cachorros',
+            imagem_bg:'banner_dog.jpg'
+        }
+    });
 };
 
 export const gatos = (req: Request, res: Response) => {
-    res.send('GATOS NO CONTROLLER');
-    //res.render('pages/page');
+    res.render('pages/page',{
+        banner:{
+            titulo:'Todos os Gatos',
+            imagem_bg:'banner_cat.jpg'
+        }
+    });
 };
 
 export const peixes = (req: Request, res: Response) => {
-    res.send('PEIXES NO CONTROLLER');
-    //res.render('pages/page');
+    res.render('pages/page',{
+        banner:{
+            titulo:'Todos os Peixes',
+            imagem_bg:'banner_fish.jpg'
+        }
+    });
 };
